018365bf7d37db99b819cdc450ceea8be9eb6f77
Alex <alex@gu3.me>
2023-02-18 20:51:26 +0000

    Basic merc resources

---
+++ code/Systems/Mercs/MercResource.cs
@@ -0,0 +1,9 @@
+﻿namespace Territory;
+
+[GameResource( "Mercenary", "merc", "Defines a mercenary." )]
+public class MercResource : GameResource
+{
+	public string Name { get; set; } = "Mercenary";
+	public int Health { get; set; } = 100;
+	public int BaseSpeed { get; set; } = 320;
+}
