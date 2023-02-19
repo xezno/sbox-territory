ec2fdc9de6bff6bf706698025bb723ed8dd4d7bb
Alex <alex@gu3.me>
2023-02-19 17:02:05 +0000

    Give mercs a type (medic, assault, etc) and a description field

--- code/Systems/Mercs/MercResource.cs
+++ code/Systems/Mercs/MercResource.cs
@@ -4,8 +4,11 @@
 public class MercResource : GameResource
 {
 	public string Name { get; set; } = "Mercenary";
+	public string Description { get; set; } = "Lorem Ipsum Dolor Sit Amet";
 	public int Health { get; set; } = 100;
 	public int BaseSpeed { get; set; } = 320;
+
+	public MercType Type { get; set; } = MercType.Assault;
 
 	public Prefab PrimaryWeapon { get; set; }
 	public Prefab SecondaryWeapon { get; set; }
---
+++ code/Systems/Mercs/MercType.cs
@@ -0,0 +1,50 @@
+﻿namespace Territory;
+
+public enum MercType
+{
+	Medic,
+	Assault,
+	Ranged,
+	Engineer,
+	FireSupport
+}
+
+public static class MercTypeExtensions
+{
+	public static string ToDisplayString( this MercType mercType )
+	{
+		var original = mercType.ToString();
+		var result = "";
+
+		for ( int i = 0; i < original.Length; ++i )
+		{
+			char c = original[i];
+			if ( i != 0 && char.IsUpper( c ) )
+				result += " ";
+
+			result += c;
+		}
+
+		return result;
+	}
+
+	public static string ToIcon( this MercType mercType )
+	{
+		// Returns a material icon name for this merc
+		switch ( mercType )
+		{
+			case MercType.Medic:
+				return "medication";
+			case MercType.Assault:
+				return "swap_vert";
+			case MercType.Ranged:
+				return "data_saver_on";
+			case MercType.Engineer:
+				return "construction";
+			case MercType.FireSupport:
+				return "local_fire_department";
+		}
+
+		return "warning";
+	}
+}
